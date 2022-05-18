e6f7f6fde8d2ada3ac2c6e1fe665756f9ee8d8c1
= <=>
2022-05-18 21:27:28 +0100

    Added entities and database config

--- src/enrollment/enrollment.module.ts
+++ src/enrollment/enrollment.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EnrollmentController } from './controller/enrollment/enrollment.controller';
 import { StudentController } from './controller/student/student.controller';
+import { Enrollment, Faculty, Student } from './entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Enrollment, Student, Faculty])],
   controllers: [EnrollmentController, StudentController],
 })
 export class EnrollmentModule {}
